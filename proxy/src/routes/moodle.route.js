f91d4fab5f14151de6cb91cdb957d0d31d7e8f1e
Ilias Almerekov <ilias.almerekov@db-n.com>
2025-10-08 20:06:29 +0200

    feat: add new function to get users

--- proxy/src/routes/moodle.route.js
+++ proxy/src/routes/moodle.route.js
@@ -1,4 +1,10 @@
+import fastify from "fastify";
 import config from "../config/env.js";
+import {
+  getSiteInfo,
+  getUserInfo,
+  getUserCourses,
+} from "../services/moodle.service.js";
 
 /**
  * @param {import('fastify').FastifyInstance} fastify
@@ -27,3 +33,35 @@ export default async function moodleRoutes(fastify) {
     }
   });
 }
+
+fastify.get("/moodle/site-info", async (request, reply) => {
+  try {
+    const info = await getSiteInfo();
+    return { status: "ok", data: info };
+  } catch (error) {
+    reply.code(500);
+    return { status: "error", message: error.message };
+  }
+});
+
+fastify.get("moodle/user/:userId", async (request, reply) => {
+  try {
+    const userId = request.params.userId;
+    const user = await getUserInfo(userId);
+    return { status: "ok", data: user };
+  } catch (error) {
+    reply.code(500);
+    return { status: "error", message: error.message };
+  }
+});
+
+fastify.get("moodle/user/:userId/courses", async (request, reply) => {
+  try {
+    const userId = request.params.userId;
+    const courses = await getUserCourses(userId);
+    return { status: "ok", data: courses };
+  } catch (error) {
+    reply.code(500);
+    return { status: "error", message: error.message };
+  }
+});
